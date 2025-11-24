30b685a9995aef5a7e18daf38e289463d01f82e3
Tom Tzook <tomtzook@gmail.com>
2025-11-24 11:57:58 +0200

    added decls

--- include/crt.h
+++ include/crt.h
@@ -14,3 +14,18 @@ void* operator new(size_t, size_t) noexcept;
 inline void* operator new(size_t, void* ptr) noexcept { return ptr; }
 inline void operator delete(void*, void*) noexcept {}
 #endif
+
+#ifdef _ministl_define_intrinsic_
+
+extern "C"
+int memcmp(const void*, const void*, size_t);
+extern "C"
+void* memset(void*, int, size_t);
+extern "C"
+void* memcpy(void*, const void*, size_t);
+extern "C"
+size_t strlen(const char* s);
+extern "C"
+int strcmp(const char* s1, const char* s2);
+
+#endif
